f954b89b32acf3b9ecc1a9ea9aaabeaf7c6102f0
jakubswi <jakubswidlowski@gmail.com>
2024-03-01 16:36:13 +0100

    fixing contact bug

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from email.mime.text import MIMEText
+from email.header import Header
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
 import smtplib
@@ -7,8 +10,6 @@ app = Flask(__name__)
 Bootstrap5(app)
 MAIL_ADDRESS = os.environ.get("EMAIL_KEY")
 MAIL_APP_PW = os.environ.get("PASSWORD_KEY")
-
-
 @app.route('/')
 def main_page():
     return render_template("index.html")
@@ -30,10 +31,14 @@ def contact():
 
 def send_email(name, email, phone, message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    email_message = MIMEText(email_message, 'plain', 'utf-8')
+    email_message['From'] = MAIL_ADDRESS
+    email_message['To'] = MAIL_ADDRESS
+    email_message['Subject'] = Header('New Message', 'utf-8')
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
-        connection.sendmail(MAIL_ADDRESS, MAIL_ADDRESS, email_message)
+        connection.sendmail(MAIL_ADDRESS, MAIL_ADDRESS, email_message.as_string())
 
 
 if __name__ == "__main__":
